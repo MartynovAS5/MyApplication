fd4f7964cf85c5159bb6c842a3748687108eccea
MartynovAS5 <amartynov700@gmail.com>
2023-05-26 16:47:46 +0300

    LB6

---
+++ myapplication5/src/main/java/com/martynov/myapplication5/MainActivity.java
@@ -0,0 +1,21 @@
+package com.martynov.myapplication5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView myTextView1 = findViewById(R.id.myText);
+        myTextView1.setText ("New text in TextView");
+        Button myBtn = findViewById(R.id.myBtn);
+        myBtn.setText ("My Button");
+        myBtn.setEnabled(false);
+    }
+}
